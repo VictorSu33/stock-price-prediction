b609ef8d8c50492e9551a5b980184ce9480a4f24
AndrewTaoWen <andrewwen6@gmail.com>
2023-09-16 16:08:13 -0400

    model created

--- stock_prediction.py
+++ stock_prediction.py
@@ -24,9 +24,11 @@ while True:
 ticker_info = ticker.info
 
 # Download historical data as dataframe
-end_date = datetime.datetime.now().date()
-start_date = end_date - datetime.timedelta(days=365*10)
+end_date = datetime.datetime.now().date() - datetime.timedelta(days=365)
+start_date = end_date - datetime.timedelta(days=365*11)
 df = yf.download(t, start=start_date, end=end_date)
+
+test_data = yf.download(t,start=datetime.datetime.now().date()-datetime.timedelta(days=365),end=datetime.datetime.now())
 
 # Calculate the moving average
 df['Moving_Avg'] = df['Close'].rolling(window=5).mean()
@@ -45,62 +47,49 @@ plt.ylabel('Price')
 plt.legend(['Close', 'Moving_Avg'], loc='upper left')
 plt.show()
 
-# Data Preprocessing
+# Data Preprocessing for Full Model Training
 X = df[['Open', 'High', 'Low', 'Volume']]
 y = df['Close']
 
-# Train-Test Split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Model Training
+# Model Training on entire data
 model = RandomForestRegressor(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
+model.fit(X, y)
 
-# Prediction
-y_pred = model.predict(X_test)
-
-# Model Evaluation
-mse = mean_squared_error(y_test, y_pred)
-rmse = np.sqrt(mse)
-print(f"Root Mean Squared Error: {rmse}")
-
-# Visualization
-plt.figure(figsize=(14, 7))
-plt.plot(y_test.index, y_test.values, label='Actual')
-plt.plot(y_test.index, y_pred, label='Predicted')
-plt.title(f"{name} Stock Price Prediction")
-plt.xlabel('Date')
-plt.ylabel('Close Price')
-plt.legend()
-plt.show()
-
-# print(type(y_test.iloc))
-
-date_to_predict = "9999-99-99" # Default value
+date_to_predict = "9999-99-99"  # Default value
 make_more = True
+# Date Validation
+today = pd.Timestamp.today().normalize()  # Get today's date and normalize time to midnight
+one_year_ago = today - pd.DateOffset(years=1)  # Date one year ago
 
 while make_more:
     while True:  # Keep running the loop until a valid date is entered
-        date_to_predict = input("Give date to perform prediction (YYYY-MM-DD):\n")
+        date_input = input("Give day from the past year to perform prediction (YYYY-MM-DD):\n")
+        date_to_predict = pd.to_datetime(date_input, errors='coerce').normalize()
 
-        if date_to_predict in y_test.index:  # Check if date exists
-            index_of_date = y_test.index.get_loc(date_to_predict)  # This should not fail now
+        if date_to_predict is pd.NaT:
+            print("Invalid date format. Please try again.")
+            continue
 
-            # Retrieve the actual and predicted value for that date
-            actual_value = y_test.iloc[index_of_date]
-            predicted_value = y_pred[index_of_date]
+        if date_to_predict < one_year_ago or date_to_predict > today:
+            print("Please enter a date within the last year.")
+            continue
 
-            # Print the results
-            print(f"Actual close price on {date_to_predict}: {actual_value}")
-            print(f"Predicted close price on {date_to_predict}: {predicted_value}")
-
-            # Calculate the error
-            error = abs(actual_value - predicted_value)
-            print(f"Absolute error: {error}")
-            
-            break  # Exit the loop since a valid date was found and processed
-        else:
+        # Create feature vector for the given date
+        try:
+            features = test_data.loc[date_to_predict][['Open', 'High', 'Low', 'Volume']]
+        except KeyError:
             print(f"Data for {date_to_predict} not found in the dataset. Please try again.")
+            continue
+
+        features = np.array(features).reshape(1, -1)  # Reshape for single prediction
+
+        # Predict the close price for that day
+        predicted_value = model.predict(features)
+
+        # Print the results
+        print(f"Predicted close price on {date_to_predict}: {predicted_value[0]}")
+
+        break  # Exit the loop since a valid date was found and processed
 
     predict_another = input("Make another prediction? (Y/N):\n")
     if predict_another == "N":
